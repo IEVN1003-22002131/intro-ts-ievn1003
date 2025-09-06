88fef168bd8417ea18f5c66481a0dcaf6f0c9d62
CarlosAbelSalinasdeJesus <cxsalinas12@gmail.com>
2025-09-05 18:45:00 -0600

    Segundo Commit 05-09-25

---
+++ src/temasTS/01-tipos-bas.ts
@@ -0,0 +1,17 @@
+console.log("Hola Mundo")
+
+var num1=23;
+var num2=32;
+const num3=67;
+
+let nombre:string
+let num:number
+let activo:boolean
+
+let sum=num1+num2+num3;
+console.log("La suma es:" + sum);
+
+let matricula:number|string;
+matricula=12345;
+matricula="Salinas"
+console.log(matricula);
